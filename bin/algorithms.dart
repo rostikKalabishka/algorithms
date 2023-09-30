c1b443103a007ef2918ff4ff8eaa09805768c87f
rostikKalabishka <rostik31052002@gmail.com>
2023-09-30 16:12:59 +0300

    change

--- bin/algorithms.dart
+++ bin/algorithms.dart
@@ -2,5 +2,5 @@ import 'binary_search/binary_search.dart';
 
 void main() {
   final solution = Solution();
-  print(solution.binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+  print(solution.binarySearch([-1, 0, 3, 5, 9, 12], 9));
 }
--- bin/binary_search/binary_search.dart
+++ bin/binary_search/binary_search.dart
@@ -1,17 +1,20 @@
 class Solution {
-  int binarySearch(List<int> sortedList, int target) {
-    int low = 0;
-    int high = sortedList.length - 1;
-    while (low <= high) {
-      int mid = (low + high);
-      if (sortedList[mid] == target) {
-        return mid;
-      } else if (sortedList[mid] < target) {
-        low = mid + 1;
+  int binarySearch(List<int> nums, int target) {
+    nums.sort();
+    int left = 0;
+    int right = nums.length - 1;
+
+    while (left <= right) {
+      int medium = ((left + right) / 2).toInt();
+      if (nums[medium] == target) {
+        return medium;
+      } else if (nums[medium] > target) {
+        right = medium - 1;
       } else {
-        high = mid - 1;
+        left = medium + 1;
       }
     }
+
     return -1;
   }
 }
