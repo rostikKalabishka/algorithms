3a2075069ede090148149dbc1b1816113b24ff2a
rostikKalabishka <rostik31052002@gmail.com>
2023-10-07 21:11:18 +0300

    missingNumber

--- bin/algorithms.dart
+++ bin/algorithms.dart
@@ -3,7 +3,12 @@ import 'binary_search/binary_search.dart';
 void main() {
   final solution = Solution();
   // print(solution.binarySearch([-1, 0, 3, 5, 9, 12], 9));
-  print(solution.removeElement([3, 2, 2, 3], 3));
-  print(solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
-  print(solution.removeElement([3, 3], 3));
+  // print(solution.removeElement([3, 2, 2, 3], 3));
+  // print(solution.removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+  // print(solution.removeElement([3, 3], 3));
+  // print(solution.longestCommonPrefix(["flower", "flow", "flight"]));
+
+  print(solution.missingNumber([3, 0, 1]));
+  print(solution.missingNumber([0, 1]));
+  print(solution.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
 }
--- bin/binary_search/binary_search.dart
+++ bin/binary_search/binary_search.dart
@@ -35,4 +35,19 @@ class Solution {
   //   }
   //   return t;
   // }
+
+  int missingNumber(List<int> nums) {
+    int value = 0;
+    for (int i = 0; i <= nums.length; i++) {
+      if (!nums.contains(i)) {
+        value = i;
+      }
+    }
+    return value;
+  }
+
+  // longestCommonPrefix(List<String> strs) {
+  //   final list1 = strs.map((e) => e.startsWith('fl'));
+  //   return list1;
+  // }
 }
