c61e82008f7e2e2d4970e522fc80765a55c7ecef
rostikKalabishka <rostik31052002@gmail.com>
2023-10-18 19:51:12 +0300

    stringMatching

--- bin/binary_search/binary_search.dart
+++ bin/binary_search/binary_search.dart
@@ -812,4 +812,16 @@ class Solution {
     }
     return counterOperations;
   }
+
+  List<String> stringMatching(List<String> words) {
+    Set<String> result = {};
+    for (int i = 0; i < words.length; i++) {
+      for (int j = 0; j < words.length; j++) {
+        if (i != j && words[i].contains(words[j])) {
+          result.add(words[j]);
+        }
+      }
+    }
+    return result.toList();
+  }
 }
