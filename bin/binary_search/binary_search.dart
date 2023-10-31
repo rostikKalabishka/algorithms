35b378771b5d51da1970f1339709efec376dfbc6
rostikKalabishka <rostik31052002@gmail.com>
2023-10-31 14:59:16 +0200

    findMedianSortedArrays

--- bin/binary_search/binary_search.dart
+++ bin/binary_search/binary_search.dart
@@ -1243,6 +1243,21 @@ class Solution {
 
     return count;
   }
+
+  double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
+    final nums = <int>[...nums1, ...nums2]..sort();
+    final middle = nums.length ~/ 2; //Rounded integer middle
+
+    if (middle == nums.length / 2) {
+      //Is rounded middle == real middle
+      int x = middle;
+      int y = middle - 1;
+      return (nums[x] + nums[y]) / 2;
+    } else {
+      int x = middle;
+      return nums[x].toDouble();
+    }
+  }
 }
  
 // class NumArray {
