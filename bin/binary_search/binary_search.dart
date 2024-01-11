84e17b271a780cbf615e21795637d282e510a447
RostikKalabishka <rostik31052002@gmail.com>
2024-01-11 14:54:44 +0200

    fix

--- bin/binary_search/binary_search.dart
+++ bin/binary_search/binary_search.dart
@@ -1322,8 +1322,11 @@ class Solution {
     if (n < 1) {
       return false;
     }
-    while (n % 3 != 0) {
-      return true;
+    while (n > 1) {
+      if (n % 3 != 0) {
+        return false;
+      }
+      n = n ~/ 3;
     }
 
     return true;
